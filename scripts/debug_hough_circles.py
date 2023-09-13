4cf5b97edc417c7e0c5d68dc6b9fbf6fd58fa3c9
Andreas Ziegler <andreas.ziegler@uni-tuebingen.de>
2023-09-13 16:11:34 +0200

    Added frame_number parameter.

--- scripts/debug_hough_circles.py
+++ scripts/debug_hough_circles.py
@@ -1,11 +1,24 @@
+import argparse
 import cv2 as cv
+import glob
 import numpy as np
 import os
 
+parser = argparse.ArgumentParser('Tune blob detector')
+parser.add_argument('--frame_number', '-fn', type=int, default=0,
+        help='The frame number to start with.')
+args = parser.parse_args()
+
 directory = "/data/ros_ws/calibration_ws/debug_frames/"
 
-for filename in os.listdir(directory):
-    file = os.path.join(directory, filename)
+# for filename in os.listdir(directory):
+file_list =  glob.iglob(directory + '/*.png')
+for file in sorted(file_list):
+    frame_number = int(file[-9:-4])
+    if frame_number < args.frame_number:
+        continue
+
+    # file = os.path.join(directory, filename)
     # checking if it is a file
     if os.path.isfile(file):
         image = cv.imread(file, cv.IMREAD_COLOR)
@@ -20,11 +33,11 @@ for filename in os.listdir(directory):
 
         if circles is not None:
             for i in circles[0, :]:
-                center = (i[0], i[1])
+                center = (int(i[0]), int(i[1]))
                 # circle center
                 cv.circle(image, center, 1, (0, 100, 100), 1)
                 # circle outline
-                radius = i[2]
+                radius = int(i[2])
                 cv.circle(image, center, radius, (255, 0, 255), 1)
 
             if len(circles[0]) == 3:
